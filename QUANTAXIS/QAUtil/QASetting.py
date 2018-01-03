78e1675069582863cfd1dc668e539e6b6ab9acb3
xujun <xujun.thu@gmail.com>
2018-01-03 22:53:46 +0800

    mongodb with auth.

--- QUANTAXIS/QAUtil/QASetting.py
+++ QUANTAXIS/QAUtil/QASetting.py
@@ -27,10 +27,10 @@ from QUANTAXIS.QAUtil import QA_util_log_info, QA_util_sql_mongo_setting
 
 class QA_Setting():
 
-    QA_util_sql_mongo_ip = '127.0.0.1'
+    QA_util_sql_mongo_ip = '192.168.8.2'
     QA_util_sql_mongo_port = '27017'
     client = QA_util_sql_mongo_setting(
-        QA_util_sql_mongo_ip, QA_util_sql_mongo_port, user_pwd="XXXXXX")
+        QA_util_sql_mongo_ip, QA_util_sql_mongo_port)
 
     QA_setting_user_name = ''
     QA_setting_user_password = ''
